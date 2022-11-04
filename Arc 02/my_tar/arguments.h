7c2807e7b477beffd48fbb9689998bae50063456
alfimois <alfimois@gmail.com>
2022-11-04 14:26:21 +0200

    remove unused headers

--- Arc 02/my_tar/arguments.h
+++ Arc 02/my_tar/arguments.h
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #include "./helpers.h"
 #include "./array.h"
 
--- Arc 02/my_tar/my_tar.c
+++ Arc 02/my_tar/my_tar.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-
 #include "./arguments.h"
-#include "./hash-table.h"
-#include "./array.h"
 
 int main (int argc, char** argv) {
   arguments_t* arguments = parse_arguments(argc, argv);
