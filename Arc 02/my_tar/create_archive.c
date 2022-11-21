da85daa04d14c89368b6d4a8aa9469b6d0c16796
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-21 15:56:50 -0800

    Load from filenames
    In create_archive.c, initialize a single tar file from Array *filenames
    Create file descriptor for reading file contents

--- Arc 02/my_tar/create_archive.c
+++ Arc 02/my_tar/create_archive.c
@@ -2,39 +2,71 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 #include "./arguments.h"
 #include "./helpers.h"
 #include "./tar-archive.h"
 #include "./tar-file.h"
 #include "./t_file_info.h"
 
-int create_archive(char *tar_filename, Array *filenames) {
-  int index = 0;
-  Tar_file *first_file = create_tar_file();
-  
-  //Create header
+int create_header(tar_header_ptr tar_file_header, char *filename) {
   file_info f_info = t_file_constructor();
   t_file_initialize(f_info);
-  f_info = get_file_info(f_info, filenames->items[0]);  //store 1 filename
-  fill_tar_header(&first_file->header, f_info);
+  f_info = get_file_info(f_info, filename);
+  if(fill_tar_header(tar_file_header, f_info) == 1) {
+    print_message(STDERR_FILENO, "Error creating header\n");
+    return 1;
+  }
+  t_file_destructor(f_info);
+  return 0;
+}
 
-  //Fill content
-  
+int read_file_contents(string_t content, char *filename, char *file_size) {
+  int size = atoi(file_size);
+  int fd = open(filename, O_RDONLY);
+  if(fd == -1) {
+    print_message(STDERR_FILENO, "Error: cannot read file\n");
+    return 1;
+  }
+
+  return 0;
+}
+
+Tar_file *load_from_filenames(Tar_file *this, Array *filenames) {
+  this = create_tar_file();
+
+  //Creates header from single filename only
+  if(create_header(&this->header, filenames->items[0]) > 0) {
+    Tar_file__free(this);
+  }
+  return this;
+}
+
+int create_archive(char *tar_filename, Array *filenames) {
   return 0;
 }
 
 int main(int argc, char **argv) {
   char *tar_name;
   Array *filename = create_array();
+  Tar_file *files;
   if(argc < 3) {
     printf("ERROR: Must enter a tar name and file name\n");
     return 1;
   }
   tar_name = strdup(argv[1]);
   Array__push(filename, argv[2]);
+  files = load_from_filenames(files, filename);
+  // if(load_from_filenames(files, filename) != 1) {
+  //   print_message(STDOUT_FILENO, "load from filename successful!\n");
+  // }
+  // else {
+  //   print_message(STDERR_FILENO, "load from filename NOT sucessful\n");
+  //   return 1;
+  // }
 
-  create_archive(tar_name, filename);
-
+  printf("%s\n", files->header.name);
+  Tar_file__free(files);
   free(tar_name);
   Array__free(filename);
   return 0;
--- Arc 02/my_tar/create_archive.h
+++ Arc 02/my_tar/create_archive.h
@@ -4,4 +4,6 @@
 #include "./tar-file.h"
 #include "./t_file_info.h"
 
+int create_header(tar_header_ptr tar_file_header, char *filename);
+int load_from_filenames(Tar_file *this, Array *filenames);
 int create_archive(char *tar_filename, Array *filenames);
