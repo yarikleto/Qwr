e5f844dc6b621b5c22f1163834db546f3f2be0f9
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-22 15:59:34 -0800

    Add build_tar_files and read_file_contents prototypes

--- Arc 02/my_tar/create_archive.h
+++ Arc 02/my_tar/create_archive.h
@@ -5,5 +5,7 @@
 #include "./t_file_info.h"
 
 int create_header(tar_header_ptr tar_file_header, char *filename);
+int read_file_contents(string_t content, char *filename, char *file_size);
+Tar_file *build_tar_file(Tar_file *this, char *filename, Tar_file *next, Tar_file *prev);
 int load_from_filenames(Tar_file *this, Array *filenames);
 int create_archive(char *tar_filename, Array *filenames);
