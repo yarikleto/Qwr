cb0943b232f9c77185e1f2a4710204242c6d4f0a
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-23 21:49:01 -0800

    Add two test cases for create_archive unit tests
    Test case 1: 3 regular files < 10240 bytes
    Test case 2: 4 regular files > 10240 bytes

--- Arc 02/my_tar/test_create_archive.sh
+++ Arc 02/my_tar/test_create_archive.sh
@@ -1,11 +1,27 @@
+echo DELETING CREATE_ARCHIVE.TAR, TAR1.TAR, CREATE_ARCHIVE.TXT, TAR1.TXT
 rm create_archive.tar
 rm tar1.tar
 rm create_archive.txt
 rm tar1.txt
-tar -cf tar1.tar test-3.txt tar-file.c array.c
 make re
+echo TEST CASE 1: tar1.tar test-3.txt tar-file.c array.c
+tar -cf tar1.tar test-3.txt tar-file.c array.c
 ./my_tar -cf create_archive.tar test-3.txt tar-file.c array.c
 cat -e tar1.tar > tar1.txt
 cat -e create_archive.tar > create_archive.txt
-echo diff tar1.txt and create_archive.txt
+echo DIFF TAR1.TXT AND CREATE_ARCHIVE.TXT
 diff tar1.txt create_archive.txt
+echo TEST CASE 1 COMPLETE
+
+echo TEST CASE 2: tar1.tar test-3.txt tar-file.c array.c create_archive.c
+rm create_archive.tar
+rm tar1.tar
+rm create_archive.txt
+rm tar1.txt
+tar -cf tar1.tar test-3.txt tar-file.c array.c create_archive.c
+./my_tar -cf create_archive.tar test-3.txt tar-file.c array.c create_archive.c
+cat -e tar1.tar > tar1.txt
+cat -e create_archive.tar > create_archive.txt
+echo DIFF TAR1.TXT AND CREATE_ARCHIVE.TXT
+diff tar1.txt create_archive.txt
+echo TEST CASE 2 COMPLETE
