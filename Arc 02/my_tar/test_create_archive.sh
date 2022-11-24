285c1b12126ae0bf51afd26175378001d027e496
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-23 22:33:54 -0800

    Add 3rd test case

--- Arc 02/my_tar/test_create_archive.sh
+++ Arc 02/my_tar/test_create_archive.sh
@@ -1,3 +1,4 @@
+#First test case: 3 regular files; tar file size = 10240 bytes
 echo DELETING CREATE_ARCHIVE.TAR, TAR1.TAR, CREATE_ARCHIVE.TXT, TAR1.TXT
 rm create_archive.tar
 rm tar1.tar
@@ -13,6 +14,7 @@ echo DIFF TAR1.TXT AND CREATE_ARCHIVE.TXT
 diff tar1.txt create_archive.txt
 echo TEST CASE 1 COMPLETE
 
+#Second test case: 4 regular files; tar file size = 20480 bytes
 echo TEST CASE 2: tar1.tar test-3.txt tar-file.c array.c create_archive.c
 rm create_archive.tar
 rm tar1.tar
@@ -25,3 +27,17 @@ cat -e create_archive.tar > create_archive.txt
 echo DIFF TAR1.TXT AND CREATE_ARCHIVE.TXT
 diff tar1.txt create_archive.txt
 echo TEST CASE 2 COMPLETE
+
+#Third test case: 1 regular file, 1 hardlink file, 1 symbolic link file
+echo TEST CASE 3: tar1.tar test-3.txt test-3-hardlink test-3-symboliclink.txt
+rm create_archive.tar
+rm tar1.tar
+rm create_archive.txt
+rm tar1.txt
+tar -cf tar1.tar test-3.txt test-3-hardlink test-3-symboliclink.txt
+./my_tar -cf create_archive.tar test-3.txt test-3-hardlink test-3-symboliclink.txt
+cat -e tar1.tar > tar1.txt
+cat -e create_archive.tar > create_archive.txt
+echo DIFF TAR1.TXT AND CREATE_ARCHIVE.TXT
+diff tar1.txt create_archive.txt
+echo TEST CASE 3 COMPLETE
