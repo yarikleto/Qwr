508f8e939d8120a75ebd2a7bac054519d8bedf54
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-26 00:16:04 -0800

    Add TEST CASE 3: tar -rf [non-existent file] [existing file]

--- Arc 02/my_tar/test_append_archive.sh
+++ Arc 02/my_tar/test_append_archive.sh
@@ -14,6 +14,18 @@ echo TEST CASE 1 COMPLETE
 
 #2. tar -rf [non-existent file] [non-existent file]
 echo TEST CASE 2: tar -rf [non-existent file] [non-existent file]
+rm append_test.tar
+rm my_append_test.tar
 tar -rf my_append_test_test.tar fffffff.txt
 ./my_tar -rf my_append_test.tar fffffff.txt
 echo TEST CASE 2 COMPLETE
+
+#3. tar -rf [non-existent file] [existing file]
+echo TEST CASE 3: tar -rf [non-existent file] [existing file]
+rm append_test.tar
+rm my_append_test.tar
+tar -rf append_test.tar array.h
+./my_tar -rf my_append_test.tar array.h
+echo DIFF APPEND_TEST AND MY_APPEND_TEST
+diff append_test.tar my_append_test.tar
+echo TEST CASE 3 COMPLETE
