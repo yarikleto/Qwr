665456c2dc82c3c8b65b564bd90de2885c57338f
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-25 23:34:26 -0800

    Create a 20 block null byte file if tar_file doesn't exist
    The 20 block null byte file is created if tar_file doesn't exist
    and the input files are not provided

--- Arc 02/my_tar/append_archive.c
+++ Arc 02/my_tar/append_archive.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "./create_archive.h"
 #include "./dir_ops.h"
 
 int append_archive(char *tar_filename, Array *filenames) {
+  int file_descriptor;
+  
   if(validate_filestat(filenames) > 0) {
     return 1;
   }
 
-  //Exit if no input files
+  //Create a 20 block null byte file if tar_file doesn't exist and no input files
   if(filenames->items == NULL && in_directory(tar_filename) > 0) {
-    printf("No input files\n");
+    file_descriptor = open(tar_filename, O_RDWR | O_CREAT, 
+                        ((S_IWUSR | S_IRUSR) | (S_IRGRP | S_IWGRP) | S_IROTH));
+    char null_pad[BLOCK_SIZE] = {0};
+    for(int i = 0; i < BLOCK_FACTOR; i++) {
+      write(file_descriptor, &null_pad, BLOCK_SIZE);
+    }
+    close(file_descriptor);
     return 0;
   }
 
