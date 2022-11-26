1247c9c2ca1f5045b3d134914e41c828988cf41e
rwkliu <114320485+rwkliu@users.noreply.github.com>
2022-11-25 22:40:45 -0800

    Exit append_archive if no valid inputs, no inputs

--- Arc 02/my_tar/append_archive.c
+++ Arc 02/my_tar/append_archive.c
@@ -3,11 +3,24 @@
 #include <string.h>
 
 #include "./create_archive.h"
+#include "./dir_ops.h"
 
 int append_archive(char *tar_filename, Array *filenames) {
-  if(filenames->items == NULL) {
+  if(validate_filestat(filenames) > 0) {
+    return 1;
+  }
+
+  //Exit if no input files
+  if(filenames->items == NULL && in_directory(tar_filename) > 0) {
+    printf("No input files\n");
     return 0;
   }
+
+  //Check if tar_filename is in current directory
+  if(in_directory(tar_filename) > 0) {
+    printf("%s in directory\n", tar_filename);
+  }
+
   printf("tar mode -r entered here\n");
   printf("tar filename: %s\n", tar_filename);
   printf("first filename: %s\n", filenames->items[0]);
